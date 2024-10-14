be88c4e36605b9b79fe7b81259f23781a2129fdf
Haqq Bassit <munagahmnh@gmail.com>
2024-10-14 22:05:13 +0000

    Add lidar

--- src/wro_ws/src/robot_description/launch/display.launch.py
+++ src/wro_ws/src/robot_description/launch/display.launch.py
@@ -14,12 +14,12 @@ def generate_launch_description():
         parameters=[{'robot_description': Command(['xacro ', LaunchConfiguration('model')])}]
     )
     
-    # joint_state_publisher_node = launch_ros.actions.Node(
-    #     package='joint_state_publisher',
-    #     executable='joint_state_publisher',
-    #     name='joint_state_publisher',
-    #     arguments=[default_model_path]
-    # )
+    joint_state_publisher_node = launch_ros.actions.Node(
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        name='joint_state_publisher',
+        arguments=[default_model_path]
+    )
   
     rviz_node = launch_ros.actions.Node(
         package='rviz2',
@@ -29,22 +29,22 @@ def generate_launch_description():
         arguments=['-d', LaunchConfiguration('rvizconfig')],
     )
     
-    spawn_entity = launch_ros.actions.Node(
-        package='gazebo_ros',
-        executable='spawn_entity.py',
-        arguments=['-entity', 'robot', '-topic', 'robot_description'],
-        output='screen'
-    )
+    # spawn_entity = launch_ros.actions.Node(
+    #     package='gazebo_ros',
+    #     executable='spawn_entity.py',
+    #     arguments=['-entity', 'robot', '-topic', 'robot_description'],
+    #     output='screen'
+    # )
 
     return launch.LaunchDescription([
         launch.actions.DeclareLaunchArgument(name='model', default_value=default_model_path,
                                             description='Absolute path to robot urdf file'),
         launch.actions.DeclareLaunchArgument(name='rvizconfig', default_value=default_rviz_config_path,
                                             description='Absolute path to rviz config file'),
-        launch.actions.ExecuteProcess(cmd=['gazebo', '--verbose', '-s', 'libgazebo_ros_init.so', '-s', 'libgazebo_ros_factory.so'], 
-                                      output='screen'),
-        # joint_state_publisher_node,
+        # launch.actions.ExecuteProcess(cmd=['gazebo', '--verbose', '-s', 'libgazebo_ros_init.so', '-s', 'libgazebo_ros_factory.so'], 
+        #                               output='screen'),
+        joint_state_publisher_node,
         robot_state_publisher_node,
-        spawn_entity,
+        # spawn_entity,
         rviz_node
     ])
